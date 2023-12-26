13f1955d40bf066c78a6e56baebc85824ea70483
Chakra <chakraodba@gmail.com>
2023-12-26 11:29:52 +0530

    bug fixed

--- user_data_script.sh
+++ user_data_script.sh
@@ -8,10 +8,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
-echo \
-  "deb -y [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+deb [arch=your_architecture] https://download.docker.com/linux/ubuntu your_ubuntu_codename stable
+
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #end
